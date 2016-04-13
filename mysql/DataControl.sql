34246a722ba12d5e923684aa1adc7f1080a5bd97
ViviTurtle <vivi.langga@gmail.com>
2016-04-13 02:16:29 -0700

    Cleaned up

--- mysql/DataControl.sql
+++ mysql/DataControl.sql
@@ -5,7 +5,7 @@ GRANT EXECUTE ON Speed_Rider.* TO 'Speed_Rider_User'@'localhost';
 GRANT INSERT ON Speed_Rider.* TO 'Speed_Rider_User'@'localhost';
 GRANT UPDATE ON Speed_Rider.* TO 'Speed_Rider_User'@'localhost';
 
--- Add Stored Procedures here
+-- Add Stored Procedures Permissions here
 -- GRANT EXECUTE ON PROCEDURE Speed_Rider.[INSERT SP HERE] TO 'Speed_Rider_User'@'localhost';-- 
 
 FLUSH PRIVILEGES;
