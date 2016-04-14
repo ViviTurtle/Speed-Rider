f124dd16a394f50fbbed9016d67ebb4d3fab16e9
James Lane <james.r.lane@sjsu.edu>
2016-04-13 22:18:58 -0700

    adding hardcoded sample drivers

--- map.js
+++ map.js
@@ -1,10 +1,51 @@
+
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 0, lng: 0},
+    center: {lat: 37.670591, lng: -122.302173}, // center in Bay Area
     zoom: 6
   });
   
   var infoWindow = new google.maps.InfoWindow({map: map});
+    
+  // Hardcoded driver coordinates
+  var sanRamonCoordinates = new google.maps.LatLng(37.729057,-121.934515);
+  var sanJoseCoordinates = new google.maps.LatLng(37.335204,-121.881029);
+  var sanMateoCoordinates = new google.maps.LatLng(37.537590,-122.298748);
+  var sanFranciscoCoordinates = new google.maps.LatLng(37.799308,-122.397434);
+  var haywardCoordinates = new google.maps.LatLng(37.629494,-122.046862);
+
+  // san ramon driver
+  var sanRamonMarker = new google.maps.Marker({
+    icon: { path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW, scale: 4 },
+    draggable: true,
+    map: map });
+  sanRamonMarker.setPosition(sanRamonCoordinates);
+    
+    // san Jose driver
+  var sanJoseMarker = new google.maps.Marker({
+    icon: { path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW, scale: 4 },
+    draggable: true,
+    map: map });
+  sanJoseMarker.setPosition(sanJoseCoordinates);
+    
+    // san Francisco driver
+ var sanFranciscoMarker = new google.maps.Marker({
+    icon: { path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW, scale: 4 },
+    draggable: true,
+    map: map });
+  sanFranciscoMarker.setPosition(sanFranciscoCoordinates);
+    
+    // san Mateo
+     var sanMateoMarker = new google.maps.Marker({
+    icon: { path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW, scale: 4 },
+    draggable: true,
+    map: map });
+  sanMateoMarker.setPosition(sanMateoCoordinates);
+
+
+    
+    
+    
 
   // Try HTML5 geolocation.
   if (navigator.geolocation) {
@@ -25,6 +66,8 @@ function initMap() {
     handleLocationError(false, infoWindow, map.getCenter());
   }
 }
+
+
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
