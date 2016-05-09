416b050d52ff85bdd70fe224e2723963748186bc
Joseph Hu <who_joey@yahoo.com>
2016-05-09 13:52:53 -0700

    Added Anh's map to driver.php

--- php/driver.php
+++ php/driver.php
@@ -7,7 +7,75 @@ include("../includes/headerL.php");
 ?>
 
 
+<style>
+    html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+    #map {
+        height: 100%;
+    }
+    #floating-panel {
+        position: absolute;
+        top: 10px;
+        left: 25%;
+        z-index: 5;
+        background-color: #fff;
+        padding: 5px;
+        border: 1px solid #999;
+        text-align: center;
+        font-family: 'Roboto','sans-serif';
+        line-height: 30px;
+        padding-left: 5px;
+    }
+    #right-panel {
+        font-family: 'Roboto','sans-serif';
+        line-height: 30px;
+        padding-left: 10px;
+    }
 
+    #right-panel select, #right-panel input {
+        font-size: 15px;
+    }
+
+    #right-panel select {
+        width: 100%;
+    }
+
+    #right-panel i {
+        font-size: 12px;
+    }
+    #right-panel {
+        height: 100%;
+        float: right;
+        width: 390px;
+        overflow: auto;
+    }
+    #map {
+        margin-right: 400px;
+    }
+    #floating-panel {
+        background: #fff;
+        padding: 1px;
+        font-size: 22px;
+        font-family: Arial;
+        border: 1px solid #ccc;
+        box-shadow: 0 2px 2px rgba(33, 33, 33, 0.4);
+        display: none;
+    }
+    @media print {
+        #map {
+            height: 500px;
+            margin: 0;
+        }
+        #right-panel {
+            float: none;
+            width: auto;
+        }
+    }
+
+</style>
 
 <header id="top" class="header">
 
@@ -18,159 +86,72 @@ include("../includes/headerL.php");
 <!-- About -->
 <section id="about" class="about">
 
-    <input id="pac-input" class="controls" type="text"
-           placeholder="Enter a location">
-    <div id="type-selector" class="controls">
-        <input type="radio" name="type" id="changetype-all" checked="checked">
-        <label for="changetype-all">All</label>
-
-        <input type="radio" name="type" id="changetype-establishment">
-        <label for="changetype-establishment">Establishments</label>
-
-        <input type="radio" name="type" id="changetype-address">
-        <label for="changetype-address">Addresses</label>
-
-        <input type="radio" name="type" id="changetype-geocode">
-        <label for="changetype-geocode">Geocodes</label>
+    <div id="floating-panel">
+        <button id ="click">Get me to my Client!</button>
     </div>
+    <div id="right-panel"></div>
+
     <div id="map"></div>
 
     <script>
+            function initMap() {
+                var directionsService = new google.maps.DirectionsService;
+                var directionsDisplay = new google.maps.DirectionsRenderer;
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 10,
+                    center: {lat: 37.3351420, lng: -121.8811}
+                });
+                directionsDisplay.setMap(map);
+                directionsDisplay.setPanel(document.getElementById('right-panel'));
 
-        /**
-         * Initialization of Grabbing Geolocation
-         */
-        function initCoords() {
-            var bayarea = new google.maps.LatLng(37.547841, -122.003326);
-            var browserSupportFlag = Boolean();
-            /*
-             Check if geolocation is allowed.
-             */
-            if (navigator.geolocation) {
-                browserSupportFlag = true;
-                navigator.geolocation.getCurrentPosition(initMap, function () {
-                    noGeolocation(browserSupportFlag);
-                });
-            } else {
-                browserSupportFlag = false;
-                noGeolocation(browserSupportFlag);
-            }
-            function noGeolocation(errorFlag) {
-                if (errorFlag == true) {
-                    alert("Geolocation service failed.");
-                    initMap(bayarea);
+                var control = document.getElementById('floating-panel');
+                control.style.display = 'block';
+                map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+
+                var infoWindow = new google.maps.InfoWindow({map: map});
+
+                var pos;
+                // Try HTML5 geolocation.
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(function(position) {
+                        pos = {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude
+                        };
+
+                        infoWindow.setPosition(pos);
+                        infoWindow.setContent('Location found.');
+                        map.setCenter(pos);
+                        map.setZoom(15);
+
+                    }, function() {
+                        handleLocationError(true, infoWindow, map.getCenter());
+                    });
                 } else {
-                    alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
-                    initMap(bayarea);
+                    // Browser doesn't support Geolocation
+                    handleLocationError(false, infoWindow, map.getCenter());
                 }
+                var onChangeHandler = function() {
+                    calculateAndDisplayRoute(directionsService, directionsDisplay, pos);
+                };
+                document.getElementById('click').addEventListener('click', onChangeHandler);
+                var trafficLayer = new google.maps.TrafficLayer();
+                trafficLayer.setMap(map);
+
             }
-        }
-        function initMap(position) {
-            // This Variable is to convert position into a LatLng object if it isn't already.
-            var convert;
-            var driverList = [
-                {lat: 37.20422, lng: -121.84769},
-                {lat: 37.36742, lng: -121.98267},
-                {lat: 37.33413, lng: -121.88052},
-            ];
-            /**
-             * Noticed that the geolocation callback function returns an object of position where
-             * there is this position.location.latitude and position.location.longitude. This type of
-             * object seems to not always work and gives me errors. Changed all variables instead to
-             * google.maps.LatLng() to keep consistent.
-             * */
-            if (position instanceof google.maps.LatLng) {
-                var map = new google.maps.Map(document.getElementById('map'), {
-                    center: {lat: position.lat(), lng: position.lng()},
-                    zoom: 10
-                });
-                convert = position;
-            } else {
-                var map = new google.maps.Map(document.getElementById('map'), {
-                    center: {lat: position.coords.latitude, lng: position.coords.longitude},
-                    zoom: 10
-                });
-                convert = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-            }
-            // Add layer of Traffic
-            var traffic = new google.maps.TrafficLayer();
-            traffic.setMap(map);
-            var input = /** @type {!HTMLInputElement} */(
-                document.getElementById('pac-input'));
-            var types = document.getElementById('type-selector');
-            map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-            map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
-            var autocomplete = new google.maps.places.Autocomplete(input);
-            autocomplete.bindTo('bounds', map);
-            var infowindow = new google.maps.InfoWindow();
-            // User Marker
-            var marker = new google.maps.Marker({
-                position: {lat: convert.lat(), lng: convert.lng()},
-                map: map,
-                anchorPoint: new google.maps.Point(0, -29),
-                draggable: false
-            });
-            marker.setIcon(({
-                url: '/img/VanSpriteSmall.png',
-                size: new google.maps.Size(75, 75),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(0, 32)
-            }));
-            // Driver Markers
-            for (var i = 0; i < driverList.length; i++){
-                var driverMarker = new google.maps.Marker({
-                    position: driverList[i],
-                    map: map,
-                    draggable: false
-                });
-                driverMarker.setIcon(({
-                    url: '/img/PassM.png',
-                    size: new google.maps.Size(75, 75),
-                    origin: new google.maps.Point(0, 0),
-                    anchor: new google.maps.Point(0, 32)
-                }));
-                driverMarker.setVisible(true);
-            }
-            autocomplete.addListener('place_changed', function () {
-                infowindow.close();
-                marker.setVisible(false);
-                var place = autocomplete.getPlace();
-                if (!place.geometry) {
-                    window.alert("Autocomplete's returned place contains no geometry");
-                    return;
-                }
-                // If the place has a geometry, then present it on a map.
-                if (place.geometry.viewport) {
-                    map.fitBounds(place.geometry.viewport);
+
+        function calculateAndDisplayRoute(directionsService, directionsDisplay, pos) {
+            directionsService.route({
+                origin: pos,
+                destination: {lat: 37.309579, lng: -121.846201},
+                travelMode: google.maps.TravelMode.DRIVING
+            }, function(response, status) {
+                if (status === google.maps.DirectionsStatus.OK) {
+                    directionsDisplay.setDirections(response);
                 } else {
-                    map.setCenter(place.geometry.location);
-                    map.setZoom(17);  // Why 17? Because it looks good.
+                    window.alert('Directions request failed due to ' + status);
                 }
-                marker.setPosition(place.geometry.location);
-                marker.setVisible(true);
-                var address = '';
-                if (place.address_components) {
-                    address = [
-                        (place.address_components[0] && place.address_components[0].short_name || ''),
-                        (place.address_components[1] && place.address_components[1].short_name || ''),
-                        (place.address_components[2] && place.address_components[2].short_name || '')
-                    ].join(' ');
-                }
-                infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-                infowindow.open(map, marker);
             });
-            // Sets a listener on a radio button to change the filter type on Places
-            // Auto-complete
-            function setupClickListener(id, types) {
-                var radioButton = document.getElementById(id);
-                radioButton.addEventListener('click', function () {
-                    autocomplete.setTypes(types);
-                });
-            }
-            setupClickListener('changetype-all', []);
-            setupClickListener('changetype-address', ['address']);
-            setupClickListener('changetype-establishment', ['establishment']);
-            setupClickListener('changetype-geocode', ['geocode']);
         }
     </script>
 
@@ -228,7 +209,7 @@ include("../includes/headerL.php");
 </footer>
 
 <!-- Maps -->
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCBJjObeyq52A4L2wUzNdUDLS6ohWWtI2c&libraries=places&callback=initCoords"
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCBJjObeyq52A4L2wUzNdUDLS6ohWWtI2c&libraries=places&callback=initMap"
         async defer></script>
 
 
