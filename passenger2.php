cd576d5ad1d54d0bf0dbbb4ab79904b09197df12
sandycomet <black_sandy54@yahoo.comgit config --global user.email black_sandy54@yahoo.comgit config --global user.email black_sandy54@yahoo.com>
2016-05-10 19:53:11 -0700

    passenger3

---
+++ passenger2.php
@@ -0,0 +1,282 @@
+<?php
+require("../includes/common.php");
+include("../includes/headerL.php");
+
+$db = new mysqli($host,$username,$password,$dbname);
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$query="CALL SP_GET_DRIV_LFJOB;";
+
+$driverL = $db->query($query);
+
+
+while ($row = mysqli_fetch_object($driverL)) {
+
+
+    $Driv[] = $row->USERNAME;
+    $Lat[] = $row->CURRENT_LATITUDE;
+    $Long[] = $row->CURRENT_LONGITUDE;
+
+}
+
+
+?>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- never cache patterns -->
+    <meta http-equiv="cache-control" content="max-age=0">
+    <meta http-equiv="cache-control" content="no-cache">
+    <meta http-equiv="expires" content="0">
+    <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT">
+    <meta http-equiv="pragma" content="no-cache">
+</head>
+<header id="top" class="header1">
+<style>
+.vcenter {
+    position: relative; 
+    top: 5%;
+    text-align: center;
+}
+#destination-input:focus {
+        border-color: #4d90fe;
+      }
+#destination-input {
+        background-color: #fff;
+        font-family: Roboto;
+        font-size: 15px;
+        font-weight: 300;
+        margin-left: 12px;
+        padding: 0 11px 0 13px;
+        text-overflow: ellipsis;
+        width: 200px;
+      }
+     
+ .controls {
+        margin-top: 10px;
+        border: 1px solid transparent;
+        border-radius: 2px 0 0 2px;
+        box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        height: 32px;
+        outline: none;
+        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+      }
+</style>
+
+<script>
+
+    
+</script>
+
+</header>
+
+
+
+<!-- About -->
+<section id="about" class="about">
+  
+    <div id="map" class="col-md-8 col-md-offset-2" style="height: 75%;">
+      </div>
+     <div class="col-md-8 col-md-offset-3" >
+     <input id="destination-input" class="controls" type="text"
+        placeholder="Enter a destination location"> 
+    </div>
+    <div class="vcenter col-md-10 col-md-offset-1 "> 
+    <form action="/php/calcCompTime.php">
+        <input type="button" value="Request a Driver" onclick="getLocationP()" class="btn-teal" style="height: 100px; width: 300px; font-size: 30px;" />
+    </form>
+    </div>
+
+   
+
+
+</section>
+
+
+
+
+<!-- Footer -->
+<footer>
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1 text-center">
+                    <h4><strong>Speed Rider Team</strong>
+                    </h4>
+                    <p>1 Washington Square<br>San Jose, CA 95192</p>
+                    <ul class="list-unstyled">
+                        <li><i class="fa fa-phone fa-fw"></i> (123) 456-7890</li>
+                        <li><i class="fa fa-envelope-o fa-fw"></i>  <a href="mailto:name@example.com">students@sjsu.edu</a>
+                        </li>
+                    </ul>
+                    <br>
+                    <ul class="list-inline">
+                        <li>
+                            <a href="#"><i class="fa fa-facebook fa-fw fa-3x"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-twitter fa-fw fa-3x"></i></a>
+                        </li>
+                    </ul>
+                    <hr class="small">
+                    <p class="text-muted">Copyright &copy; Speed Rider 2016</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</footer>
+
+
+<!-- Maps -->
+
+    <script>
+        function initMap() {
+        
+        var destination_place_id = null;
+        
+        var map = new google.maps.Map(document.getElementById('map'), {
+          mapTypeControl: false,
+          center: {lat: 37.3351420, lng: -121.8811},
+          zoom: 13
+        });
+        var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        directionsDisplay.setMap(map);
+        
+
+        // get current location
+        var infoWindow = new google.maps.InfoWindow({map: map});
+        var pos;
+                // Try HTML5 geolocation.
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(function(position) {
+                            pos = {
+                                    lat: position.coords.latitude,
+                                    lng: position.coords.longitude
+                                  };
+                                                                                           
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent('You are here.');
+                    map.setCenter(pos);
+                    map.setZoom(15);
+                                                                                           
+                    }, function() {
+                        handleLocationError(true, infoWindow, map.getCenter());
+                    });
+                    } else {
+                    // Browser doesn't support Geolocation
+                    handleLocationError(false, infoWindow, map.getCenter());
+                    }
+
+    // getting the destination. To get lat and long, use document.getElementById('destination-input'). value
+        var destination_input = document.getElementById('destination-input');
+      
+
+  //      map.controls[google.maps.ControlPosition.TOP_LEFT].push(pos);
+  //      map.controls[google.maps.ControlPosition.TOP_LEFT].push(destination_input);
+ 
+
+        var destination_autocomplete =
+            new google.maps.places.Autocomplete(destination_input);
+        destination_autocomplete.bindTo('bounds', map);
+
+        // Sets a listener on a radio button to change the filter type on Places
+
+        function expandViewportToFitPlace(map, place) {
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+          }
+        }
+
+       
+        destination_autocomplete.addListener('place_changed', function() {
+          var place = destination_autocomplete.getPlace();
+          if (!place.geometry) {
+            window.alert("Autocomplete's returned place contains no geometry");
+            return;
+          }
+          expandViewportToFitPlace(map, place);
+
+          // If the place has a geometry, store its place ID and route if we have
+          // the other place ID
+          destination_place_id = place.place_id;
+          route(pos, destination_place_id, 
+                directionsService, directionsDisplay);
+        });
+        var trafficLayer = new google.maps.TrafficLayer();
+                                              trafficLayer.setMap(map);
+        function route(pos, destination_place_id, 
+                       directionsService, directionsDisplay) {
+                   directionsService.route({
+            origin: pos,
+            destination: {'placeId': destination_place_id},
+            travelMode: google.maps.TravelMode.DRIVING
+          }, function(response, status) {
+            if (status === google.maps.DirectionsStatus.OK) {
+              directionsDisplay.setDirections(response);
+            } else {
+              window.alert('Directions request failed due to ' + status);
+            }
+          });
+        }
+      }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAAo_ycMAjDvH5v14Z595CyEIr5zbHEnFQ&libraries=places&callback=initMap"
+        async defer></script>
+
+
+<!-- jQuery -->
+<script src="/js/jquery-1.12.3.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="/js/bootstrap.min.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script>
+    // Closes the sidebar menu
+    $("#menu-close").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+
+    // Opens the sidebar menu
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+
+    // Scrolls to the selected menu item on the page
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+</script>
+
+
+
